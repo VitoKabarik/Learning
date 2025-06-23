84e1d0ec8ba47c53427af7e4baa5f29320d922d1
Виталий Москвин <iphone59perm@gmail.com>
2025-06-23 18:41:11 +0300

    Создание функции, возвращающей понятную дату. Отладка модуля vidjets

--- src/homework/widjet.py
+++ src/homework/widjet.py
@@ -1,20 +1,14 @@
-import masks
+from homework import masks
+
 
 def mask_account_card(type_and_num: str) -> str:
+    """Обрабатывает строку с номером счёта/карты, маскируя его"""
     if type_and_num[:4] == "Счет":
         return type_and_num[:5] + masks.get_mask_account(type_and_num)
     else:
         return type_and_num[:-16] + masks.get_mask_card_number(type_and_num[-16:])
 
 
-def get_date():
-    pass
-
-print(mask_account_card('Maestro 1596837868705199'))
-print(mask_account_card('Счет 64686473678894779589'))
-print(mask_account_card('MasterCard 7158300734726758'))
-print(mask_account_card('Счет 35383033474447895560'))
-print(mask_account_card('Visa Classic 6831982476737658'))
-print(mask_account_card('Visa Platinum 8990922113665229'))
-print(mask_account_card('Visa Gold 5999414228426353'))
-print(mask_account_card('Счет 73654108430135874305'))
+def get_date(row_date: str) -> str:
+    """Расшифровывает дату"""
+    return row_date[8:10] + "." + row_date[5:7] + "." + row_date[:4]
