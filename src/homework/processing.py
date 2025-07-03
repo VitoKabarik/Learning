f965dd90f57be49889bddf009bf11a67188e5c10
Виталий Москвин <iphone59perm@gmail.com>
2025-07-03 13:35:39 +0300

    Создание функции, сортирующей данные по дате

--- src/homework/processing.py
+++ src/homework/processing.py
@@ -7,3 +7,8 @@ def filter_by_state(list_of_datas: list, state='EXECUTED') -> list:
         if data['state'] == state:
             result.append(data)
     return result
+
+
+def sort_by_date(list_of_datas: list, param_of_reverse=True) -> list:
+    result = sorted(list_of_datas, key=lambda x: x['date'], reverse=param_of_reverse)
+    return result
