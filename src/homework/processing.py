e76eb46a77ec472cccceda3429c83dabab120159
Виталий Москвин <iphone59perm@gmail.com>
2025-07-05 11:54:03 +0300

    Добавление файла README. Исправления имён переменных

--- src/homework/processing.py
+++ src/homework/processing.py
@@ -1,13 +1,13 @@
-def filter_by_state(list_of_datas: list, state: str = "EXECUTED") -> list:
-    """Ищет в списке данные с определённым статусом"""
+def filter_by_state(list_of_operations: list, state: str = "EXECUTED") -> list:
+    """Ищет в списке операции с определённым статусом"""
     result = []
-    for data in list_of_datas:
+    for data in list_of_operations:
         if data["state"] == state:
             result.append(data)
     return result
 
 
-def sort_by_date(list_of_datas: list, param_of_reverse: bool = True) -> list:
-    """Сортирует данные по дате"""
-    result = sorted(list_of_datas, key=lambda x: x["date"], reverse=param_of_reverse)
+def sort_by_date(list_of_operations: list, is_reverse: bool = True) -> list:
+    """Сортирует операции по дате"""
+    result = sorted(list_of_operations, key=lambda x: x["date"], reverse=is_reverse)
     return result
