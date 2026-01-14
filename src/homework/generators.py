8bdbed132ce1949df6d1bab153854c37ba042461
Виталий Москвин <iphone59perm@gmail.com>
2026-01-14 20:43:35 +0300

    Исправление критической ошибки в поведении функции generators

--- src/homework/generators.py
+++ src/homework/generators.py
@@ -9,9 +9,7 @@ def filter_by_currency(list_of_transactions: list, currency: str) -> Iterator:
             suitable_transactions.append(list_of_transactions[i])
             yield list_of_transactions[i]
     if not suitable_transactions:
-        raise RuntimeError("Транзакций с данной валютой нет в списке")
-    else:
-        raise RuntimeError("Других транзакций с данной валютой нет в списке")
+        return "Транзакций с данной валютой нет в списке"
 
 
 def transaction_descriptions(list_of_transactions: list) -> Iterator:
@@ -24,7 +22,6 @@ def transaction_descriptions(list_of_transactions: list) -> Iterator:
                 yield list_of_transactions[i]['description']
             else:
                 yield "Тип данной транзакции неизвестен"
-        raise RuntimeError("В списке больше нет транзакций")
 
 
 def card_number_generator(start: int, stop: int) -> Iterator:
@@ -37,4 +34,3 @@ def card_number_generator(start: int, stop: int) -> Iterator:
         card_number = '0' * (16 - len(str(start))) + str(start)
         yield f'{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:16]}'
         start += 1
-    raise RuntimeError("Все возможные номера карт в указанном интервале уже сгенерированы")
