9f76049914c4bdc49ab143181ed882e3fce27022
Виталий Москвин <iphone59perm@gmail.com>
2026-01-20 21:35:38 +0300

    Оптимизация новых модулей согласно PEP8.

--- src/homework/decorators.py
+++ src/homework/decorators.py
@@ -1,26 +1,33 @@
+import time
+from collections.abc import Callable
 from functools import wraps
+from typing import Any
 
 
-def log(filename = ''):
-    def decorator(function):
+def log(filename: str="") -> Callable:
+    """Декоратор, отслеживающий работу функции."""
+
+    def decorator(function: Callable) -> Callable:
         @wraps(function)
-        def wrapper(*args, **kwargs):
-            if filename:
-                with open(filename, 'a') as file:
-                    try:
-                        result = function(*args, **kwargs)
-                    except Exception as err_mess:
-                        file.write(f'{function.__name__} error: {err_mess}. Inputs: {args}, {kwargs}\n')
-                    else:
-                        file.write(f'{function.__name__} ok.\n')
-                        return result
-            else:
-                try:
-                    result = function(*args, **kwargs)
-                except Exception as err_mess:
-                    print(f'{function.__name__} error: {err_mess}. Inputs: {args}, {kwargs}')
+        def wrapper(*args: tuple[Any], **kwargs: Any) -> Any:
+            mess_from_log = ""
+            err = None
+            try:
+                result = function(*args, **kwargs)
+                mess_from_log = f"{time.ctime()} {function.__name__} ok"
+                return result
+            except Exception as e:
+                err = e
+                mess_from_log = f"{time.ctime()} {function.__name__} {type(e).__name__}: {e}. Inputs: {args}, {kwargs}"
+            finally:
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(f"{mess_from_log}\n")
                 else:
-                    print(f'{function.__name__} ok')
-                    return result
+                    print(mess_from_log)
+                if err:
+                    raise err
+
         return wrapper
+
     return decorator
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,46 +1,54 @@
 import os
-import pytest
-from homework.decorators import log
 import tempfile
 import time
+from typing import Any, Union
+
+import pytest
+
+from homework.decorators import log
 
 
-def test_log(capsys):
-    with tempfile.NamedTemporaryFile(mode='a+t', delete=False) as logfile:
-        logfile_name = logfile.name
-    @log(logfile_name)
+def test_log(capsys: pytest.CaptureFixture[str]) -> None:
+    """Тестирует декоратор, логирующий работу функции."""
+    with tempfile.NamedTemporaryFile(mode="a+t", delete=False) as tf:
+        logfile = tf.name
+
+    @log(logfile)
     def division_with_round(a: int, b: int) -> float:
+        """Функция для проверки. Делит одно число на другое."""
         return round(a / b, 2)
-    division_with_round(17,5)
-    now = time.ctime()
-    with open(logfile_name, 'r') as logfile:
-        result_str = logfile.read()
-    assert result_str == f'{now} division_with_round ok\n'
 
-    open(logfile_name, 'w').close()
+    division_with_round(17, 5)
+    now = time.ctime()
+    with open(logfile, "r") as tfl:
+        result_str = tfl.read()
+    assert result_str == f"{now} division_with_round ok\n"
+
+    open(logfile, "w").close()
     now = time.ctime()
     with pytest.raises(ZeroDivisionError):
         division_with_round(3, 0)
-    with open(logfile_name, 'r') as logfile:
-        result_str = logfile.read()
-    assert result_str == f'{now} division_with_round ZeroDivisionError: division by zero. Inputs: (3, 0), {{}}\n'
-    if os.path.exists(logfile_name):
-        os.remove(logfile_name)
+    with open(logfile, "r") as lf:
+        result_str = lf.read()
+    assert result_str == f"{now} division_with_round ZeroDivisionError: division by zero. Inputs: (3, 0), {{}}\n"
+    if os.path.exists(logfile):
+        os.remove(logfile)
 
     @log()
-    def search_key(dictionary, the_key):
+    def search_key(dictionary: dict, the_key: Union[str | int]) -> Any:
+        """Функция для проверки. Возвращает из словаря значение по указанному ключу."""
         return dictionary[the_key]
 
-    some_dict = {'apple': 'manzana', 'water': 'agua', 'drink': 'beber', 'sun': 'Sol'}
+    some_dict = {"apple": "manzana", "water": "agua", "drink": "beber", "sun": "Sol"}
 
-    assertion_value = search_key(some_dict, 'water')
+    assertion_value = search_key(some_dict, "water")
     now = time.ctime()
     captured = capsys.readouterr()
-    assert captured.out == f'{now} search_key ok\n'
-    assert assertion_value == 'agua'
+    assert captured.out == f"{now} search_key ok\n"
+    assert assertion_value == "agua"
 
     with pytest.raises(KeyError):
-        search_key(some_dict, 'orange')
+        search_key(some_dict, "orange")
     now = time.ctime()
     captured = capsys.readouterr()
     assert captured.out == f"{now} search_key KeyError: 'orange'. Inputs: ({str(some_dict)}, 'orange'), {{}}\n"
@@ -49,17 +57,19 @@ def test_log(capsys):
         search_key()
     now = time.ctime()
     captured = capsys.readouterr()
-    assert f'{now} search_key TypeError:' in captured.out
-    assert 'test_log.<locals>.search_key() missing 2 required positional arguments:' in captured.out
+    assert f"{now} search_key TypeError:" in captured.out
+    assert "test_log.<locals>.search_key() missing 2 required positional arguments:" in captured.out
     assert "'dictionary' and 'the_key'. Inputs: (), {}\n" in captured.out
 
     @log()
-    def concatenation(str1, str2, str3, str4):
+    def concatenation(str1: str, str2: str, str3: str, str4: str) -> str:
+        """Функция для проверки. Соединяет указанные строки в одну."""
         return str1 + str2 + str3 + str4
 
     with pytest.raises(TypeError):
-        concatenation(str1='Python ', str2='is very ', str3=True, str4='language')
+        concatenation(str1="Python ", str2="is very ", str3=True, str4="language")
     now = time.ctime()
     captured = capsys.readouterr()
     assertion_kwargs = "{'str1': 'Python ', 'str2': 'is very ', 'str3': True, 'str4': 'language'}"
-    assert captured.out == f"{now} concatenation TypeError: can only concatenate str (not \"bool\") to str. Inputs: (), {assertion_kwargs}\n"
+    assert f"{now} concatenation TypeError:" in captured.out
+    assert f'can only concatenate str (not "bool") to str. Inputs: (), {assertion_kwargs}\n' in captured.out
