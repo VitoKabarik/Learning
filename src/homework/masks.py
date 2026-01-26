76ad2c9a33215a2fb93cf8c89317b419b16cd632
Виталий Москвин <iphone59perm@gmail.com>
2026-01-27 01:22:20 +0300

    Перенос части функционала из widjet в masks. Теперь проверка валидности номера карты или счёта происходит в функциях модуля masks.

--- src/homework/masks.py
+++ src/homework/masks.py
@@ -4,16 +4,27 @@ import os
 
 def get_mask_card_number(card_number: str) -> str:
     """Маскирует номер карты."""
-    result = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
-    return result
+    result = ''
+    for sym in card_number:
+        if sym in '0123456789':
+            result += sym
+    if len(result) != 16:
+        raise ValueError("Некорректный номер карты")
+    return f"{result[:4]} {result[4:6]}** **** {result[-4:]}"
 
 
 def get_mask_account(account: str) -> str:
     """Маскирует номер лицевого счёта."""
-    return "**" + account[-4:]
+    result = ''
+    for sym in account:
+        if sym in '0123456789':
+            result += sym
+    if len(result) != 20:
+        raise ValueError("Некорректный номер лицевого счёта")
+    return f"**{result[-4:]}"
 
 
-path_to_masks_log = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'logs', 'masks.log')
+path_to_masks_log = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'logs', 'masks.log')
 masks_logger = logging.getLogger(__name__)
 masks_logger.propagate = False
 f_handler_for_masks = logging.FileHandler(path_to_masks_log, mode='w', encoding='utf-8')
